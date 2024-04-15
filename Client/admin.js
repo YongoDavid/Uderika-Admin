6026113ac9d9f0f20dbda0a4b4f905007c483b49
Yongo David <davidyongo9@gmail.com>
2024-04-15 05:53:36 +0100

    url update

--- Client/admin.js
+++ Client/admin.js
@@ -1,23 +1,23 @@
 // CONVERT DATA TO JSON FORMAT 
-fetch('https://fakestoreapi.com/products').then((data)=>{
-    // console.log(data);
-    return data.json()  // DATA CONVERTED 
-}).then((objectData)=>{
-    console.log(objectData);
+// fetch('https://fakestoreapi.com/products').then((data)=>{
+//     // console.log(data);
+//     return data.json()  // DATA CONVERTED 
+// }).then((objectData)=>{
+//     console.log(objectData);
 
-    let tableData = " ";
-    objectData.map((values)=>{
-        tableData+= `
-        <tr>
-            <td>${values.title}</td>
-            <td>${values.description}</td>
-            <td>${values.price}</td>
-            <td>${values.image }</td>
-        </tr>
-        `
-    });
-    document.getElementById("table_body").
-    innerHTML=tableData;
-}).catch((err)=>{
-    alert("There is an Error");
-})
+//     let tableData = " ";
+//     objectData.map((values)=>{
+//         tableData+= `
+//         <tr>
+//             <td>${values.title}</td>
+//             <td>${values.description}</td>
+//             <td>${values.price}</td>
+//             <td>${values.image }</td>
+//         </tr>
+//         `
+//     });
+//     document.getElementById("table_body").
+//     innerHTML=tableData;
+// }).catch((err)=>{
+//     alert("There is an Error");
+// })
